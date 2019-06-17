5ef39960f4c17c6e9245dd593d81c5831d2a2258
tplinderoth <tylerp.linderoth@gmail.com>
2019-06-17 17:06:53 +0100

    calculate proportion of sites with |iHS| > cutoff

--- ihsWindow.cpp
+++ ihsWindow.cpp
@@ -10,28 +10,31 @@
 #include <string>
 #include <sstream>
 #include <cmath>
+#include <cstring>
 
-void info (unsigned int winsize) {
+void info (unsigned int winsize, double ihs_cutoff) {
 	std::cout << "\nUsage:\n"
-	<< "ihsWindow [selscan normalized iHS *.norm file] [window size (bp)]\n"
-	<< "default window size: " << winsize << "\n"
-	<< "Assumes iHS locus ID in format chr*_position\n"
+	<< "ihsWindow [selscan normalized iHS *.norm file] [options]\n"
+	<< "\nAssumes iHS locus ID in format chr*_position\n"
+	<< "\nOptions\n"
+	<< "-winsize INT Window size (bp) [" << winsize << "]\n"
+	<< "-cutoff FLOAT Determine fraction of sites with |iHS| > cutoff [" << ihs_cutoff << "]\n"
 	<< "\nOutput:\n"
 	<< "(1) chromosome\n"
 	<< "(2) window start\n"
 	<< "(3) window end\n"
 	<< "(4) most extreme iHS score\n"
 	<< "(5) extreme iHS position\n"
-	<< "(6) average |iHS|\n"
+	<< "(6) proportion |iHS| > cutoff\n"
 	<< "(7) Number SNPs in window\n\n";
 }
 
-int parseArgs (int argc, char** argv, std::ifstream &ihsfile, unsigned int &windowsz) {
+int parseArgs (int argc, char** argv, std::ifstream &ihsfile, unsigned int &windowsz, double &ihs_cutoff) {
 
         int rv = 0;
         if (argc < 2) {
                 std::cerr << "Must supply iHS file\n";
-		info(windowsz);
+		info(windowsz, ihs_cutoff);
                 return 1;
         }
 
@@ -41,13 +44,28 @@ int parseArgs (int argc, char** argv, std::ifstream &ihsfile, unsigned int &wind
                 return -1;
         }
 
-        if (argc > 2) {
-                windowsz = atoi(argv[2]);
-                if (windowsz <= 0) {
-                        std::cerr << "Window size must be a positive integer\n";
-                        return -1;
-                }
-        }
+	int argpos = 2;
+	while (argpos < argc) {
+        	if (strcmp(argv[argpos], "-winsize") == 0) {
+                	windowsz = atoi(argv[argpos+1]);
+                	if (windowsz <= 0) {
+                        	std::cerr << "Window size must be a positive integer\n";
+                        	return -1;
+                	}
+        	}
+		else if (strcmp(argv[argpos],"-cutoff") == 0) {
+			ihs_cutoff = atof(argv[argpos+1]);
+			if (ihs_cutoff < 0) {
+				std::cerr << "|iHS| cutoff must be >= zero\n";
+				return -1;
+			}
+		} else {
+			std::cerr << "Unknown argument " << argv[argpos] << "\n";
+			return -1;
+		}
+
+		argpos += 2;
+	}
 
         return rv;
 }
@@ -72,14 +90,14 @@ void updateMax (std::vector<double> &siteinfo, unsigned int pos, double* maxinfo
 	maxinfo[2] = pos;
 }
 
-void printWindow(std::string &chr, const unsigned int &winstart, const unsigned int &winend, const double &sumihs,
+void printWindow(std::string &chr, const unsigned int &winstart, const unsigned int &winend, double nbig,
 	const unsigned int &nsites, double* maxihs) {
-	double avgihs = sumihs/nsites;
+	double pbig = nbig/nsites;
 	std::cout << chr << "\t" << winstart << "\t" << winend << "\t"
-	<< maxihs[1] << "\t" << static_cast<unsigned int>(maxihs[2]) << "\t" << avgihs << "\t" << nsites << "\n";
+	<< maxihs[1] << "\t" << static_cast<unsigned int>(maxihs[2]) << "\t" << pbig << "\t" << nsites << "\n";
 }
 
-int calciHSWindows(std::ifstream &ihsstream, const unsigned int winsize) {
+int calciHSWindows(std::ifstream &ihsstream, const unsigned int winsize, const double ihs_cutoff) {
 	std::string sitestr;
 	std::stringstream ss;
 	std::vector<double> sitevec;
@@ -90,8 +108,8 @@ int calciHSWindows(std::ifstream &ihsstream, const unsigned int winsize) {
 	unsigned int pos;
 	unsigned int winstart = 0, winend = 0;
 	double maxihs [3]; // 0=|iHS|, 1=iHS, 2=position
-	double sumihs = 0;
 	unsigned int nsites = 0;
+	int nbig = 0;
 
 	while(getline(ihsstream, sitestr)) {
 		// extract site information
@@ -110,7 +128,7 @@ int calciHSWindows(std::ifstream &ihsstream, const unsigned int winsize) {
 			if (nsites == 1) {
 				winend = winstart+winsize-1;
 			}
-			printWindow(chr, winstart, winend, sumihs, nsites, maxihs);
+			printWindow(chr, winstart, winend, nbig, nsites, maxihs);
 
 			nsites = 0;
 		}
@@ -119,7 +137,7 @@ int calciHSWindows(std::ifstream &ihsstream, const unsigned int winsize) {
 		double absihs = fabs(sitevec[4]);
 		if (nsites == 0) {
 			winstart = pos;
-			sumihs = 0;
+			nbig = 0;
 			updateMax(sitevec, pos, maxihs);
 		}
 
@@ -127,14 +145,17 @@ int calciHSWindows(std::ifstream &ihsstream, const unsigned int winsize) {
 			updateMax(sitevec, pos, maxihs);
 		}
 
-		sumihs += absihs;
+		if (absihs > ihs_cutoff) {
+			++nbig;
+		}
+
 		winend = pos;
 		chr = newchr;
 		++nsites;
 	}
 
 	// print last window
-	printWindow(chr, winstart, winend, sumihs, nsites, maxihs);
+	printWindow(chr, winstart, winend, nbig, nsites, maxihs);
 
 	return 0;
 }
@@ -143,9 +164,10 @@ int main (int argc, char** argv) {
 	int rv = 0;
 	std::ifstream ihsfile;
 	unsigned int winsize = 100000;
+	double ihs_cutoff = 3;
 
-	if (!(rv=parseArgs(argc, argv, ihsfile, winsize))) {
-		calciHSWindows(ihsfile, winsize);
+	if (!(rv=parseArgs(argc, argv, ihsfile, winsize, ihs_cutoff))) {
+		calciHSWindows(ihsfile, winsize, ihs_cutoff);
 	}
 
 	if (ihsfile.is_open()) ihsfile.close();
